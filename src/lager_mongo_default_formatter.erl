a58fb10ced10cf58725d035a14a1e1c67b702916
Led <ledest@gmail.com>
2017-10-30 12:49:51 +0200

    lager_mongo_default_formatter: convert message to unicode binary

--- src/lager_mongo_default_formatter.erl
+++ src/lager_mongo_default_formatter.erl
@@ -12,7 +12,7 @@ format(Message) ->
     {<<"level">>, lager_msg:severity(Message),
      <<"time">>, M * 1000000 + S,
      <<"node">>, node(),
-     <<"msg">>, to_binary(lager_msg:message(Message)),
+     <<"msg">>, to_unicode(lager_msg:message(Message)),
      <<"_module">>, to_binary(proplists:get_value(module, Metadata)),
      <<"_line">>, proplists:get_value(line, Metadata, 0)}.
 
@@ -20,3 +20,8 @@ to_binary(B) when is_binary(B) -> B;
 to_binary(S) when is_list(S) -> list_to_binary(S);
 to_binary(A) when is_atom(A) -> atom_to_binary(A, utf8);
 to_binary(_) -> <<>>.
+
+to_unicode(B) when is_binary(B) -> B;
+to_unicode(S) when is_list(S) -> unicode:characters_to_binary(S);
+to_unicode(A) when is_atom(A) -> atom_to_binary(A, utf8);
+to_unicode(_) -> <<>>.
